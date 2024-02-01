59c936f665578ae66e7ab2649bfecca2b231d6af
Clay Mangum <clay@trinitasaccounting.com>
2024-01-31 21:29:10 -0600

    complete

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // Your code goes here
+
+
+document.addEventListener('DOMContentLoaded', () => {
+    const p = document.querySelector('#text');
+    p.textContent = 'This is really cool!'
+});
+
+
+
